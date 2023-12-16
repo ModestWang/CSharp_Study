13a3881885884aeaa0ea6b97999fc1e482bd63d8
Modest Wang <1598593280@qq.com>
2023-12-16 11:32:43 +0800

    第4次提交（控制台，BaiduPaddleOCR测试）

---
+++ BaiduPaddle/BaiduPaddle/Program.cs
@@ -0,0 +1,28 @@
+﻿using PaddleOCRSharp;
+
+namespace BaiduPaddle
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            OCRModelConfig config = null;
+            OCRParameter oCRParameter = new OCRParameter();
+            OCRResult ocrResult = new OCRResult();
+
+            using (PaddleOCREngine engine = new PaddleOCREngine(config, oCRParameter))
+            {
+                string image = "test.png";
+                ocrResult = engine.DetectText("../../../../Images" + image);
+                if (ocrResult != null)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(ocrResult.Text);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Successfully!");
+        }
+    }
+}
