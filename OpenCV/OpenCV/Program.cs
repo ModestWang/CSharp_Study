93180d14bf507812c1e13944db57b3eec3904370
Modest Wang <1598593280@qq.com>
2023-12-16 12:12:56 +0800

    第五次提交（OpenCVCSharp测试，EmgeCV和NetVips尚未测试）

---
+++ OpenCV/OpenCV/Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建一张大小为300*300颜色为绿色的三通道彩色图像
+            Mat img = new Mat(300, 300, MatType.CV_8UC3, new Scalar(0, 255, 0));
+            //显示图像
+            Cv2.ImShow("img", img);
+            //延时等待按键按下
+            Cv2.WaitKey(0);
+        }
+    }
+}
